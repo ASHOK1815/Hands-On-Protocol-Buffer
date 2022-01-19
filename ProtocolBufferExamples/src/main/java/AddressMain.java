a7ca9d2fee6028b217a44365154fa0222d2fe908
ASH1815 <kumawatashok1815@gmail.com>
2022-01-19 13:13:55 +0530

    adding_files2

---
+++ ProtocolBufferExamples/src/main/java/AddressMain.java
@@ -0,0 +1,17 @@
+import com.example.tutorial.protos.AddressBook;
+import com.example.tutorial.protos.Person;
+
+public class AddressMain {
+    public static void main(String[] args) {
+        Person john =
+                Person.newBuilder()
+                        .setId(1234)
+                        .setName("John Doe")
+                        .setEmail("jdoe@example.com")
+                        .addPhones(
+                                Person.PhoneNumber.newBuilder()
+                                        .setNumber("555-4321")
+                                        .setType(Person.PhoneType.HOME))
+                        .build();
+    }
+}
