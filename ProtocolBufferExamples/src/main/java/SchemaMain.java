443ccd99a9ac4ce29e734be0bb76a01bc62008c0
ASH1815 <kumawatashok1815@gmail.com>
2022-01-20 09:12:13 +0530

    Adding_files_5

--- ProtocolBufferExamples/src/main/java/SchemaMain.java
+++ ProtocolBufferExamples/src/main/java/SchemaMain.java
@@ -3,6 +3,8 @@
 import proto.example.Schema.Students;
 import proto.example.Schema.Student;
 
+import java.util.ArrayList;
+
 
 public class SchemaMain {
     public static void main(String[] args) {
@@ -20,7 +22,20 @@ public class SchemaMain {
                 .setUid(2467));
 
 
+        Student.Builder student = Student.newBuilder();
 
+        student.setUid(6735)
+                .setDepartment("cse")
+                .setName("ashok");
+
+        ArrayList<Students.Builder> arrayList = new ArrayList<>();
+        arrayList.add(Students.newBuilder().addStudents(student));
+
+        System.out.println("Array list ");
+        System.out.println(arrayList.toString());
+
+
+        System.out.println("Printing in message");
         Students message =students.build();
 
         System.out.println(message);
