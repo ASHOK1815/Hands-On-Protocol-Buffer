7b35a71f7af5108012e056f8e559b7ec5b1a3004
ASH1815 <kumawatashok1815@gmail.com>
2022-01-20 21:04:14 +0530

    Adding_files_employee:100:

--- ProtocolBufferExamples/src/main/java/CustomerMain.java
+++ ProtocolBufferExamples/src/main/java/CustomerMain.java
@@ -2,16 +2,37 @@
 import com.example.protobufexamplejava.Customer;
 import com.example.protobufexamplejava.Date;
 import com.example.protobufexamplejava.Money;
+import com.google.protobuf.Timestamp;
+
+
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.ZoneOffset;
 
 public class CustomerMain {
+
+    protected static Timestamp toGoogleTimestampUTC(final LocalDateTime localDateTime) {
+        return Timestamp.newBuilder()
+                .setSeconds(localDateTime.toEpochSecond(ZoneOffset.UTC))
+                .setNanos(localDateTime.getNano())
+                .build();
+    }
+
+
 
 
     public static void main(String[] args) {
 
+        Timestamp createdUpdateAt = toGoogleTimestampUTC(LocalDateTime.now());
+
+
         Customer.Builder builder = Customer.newBuilder();
         builder.setName("ASHOK")
                 .setBirthdate(Date.newBuilder().setDay(18).setMonth(8).setYear(1998))
-                .setBalance(Money.newBuilder().setCurrencyCode("IIFC").setNanos(20).setUnits(2));
+                .setBalance(Money.newBuilder().setCurrencyCode("IIFC").setNanos(20).setUnits(2))
+                .setCreatedAt(createdUpdateAt)
+                ;
 
 
         Customer message =  builder.build();
---
+++ ProtocolBufferExamples/src/main/java/EmployeeMain.java
@@ -0,0 +1,29 @@
+
+
+import com.example.protobuff.Employee;
+import com.example.protobuff.EmployeeList;
+
+public class EmployeeMain {
+
+
+    Employee.PhoneNumber check(String phoneNumber,String phoneType)
+    {
+        Employee.PhoneNumber.Builder phoneNumber1 = Employee.PhoneNumber.newBuilder();
+        phoneNumber1.setNumber(phoneNumber);
+
+
+        System.out.println();
+
+
+
+    }
+
+
+    public static void main(String[] args) {
+
+        EmployeeList.Builder employeeList = EmployeeList.newBuilder();
+
+
+
+    }
+}
