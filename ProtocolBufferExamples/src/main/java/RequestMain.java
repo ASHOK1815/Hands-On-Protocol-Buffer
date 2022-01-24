9bf2bb3335c074cf749da14b59aaddaf564bc940
ASH1815 <kumawatashok1815@gmail.com>
2022-01-24 10:14:59 +0530

    Adding_files_changes:smile:

---
+++ ProtocolBufferExamples/src/main/java/RequestMain.java
@@ -0,0 +1,14 @@
+
+
+
+public class RequestMain {
+
+    public static void main(String[] args) {
+
+        Schema.SearchRequestPackage.SearchRequest.Builder seBuilder = Schema.SearchRequestPackage.SearchRequest.newBuilder();
+
+
+
+
+    }
+}
